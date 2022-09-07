9c7d037c93e259d04633ddc4aa2f44933a49ce40
Shun Nakatani <kaicho8636@gmail.com>
2022-09-07 22:23:05 +0900

    Fix undo

--- prover.py
+++ prover.py
@@ -24,8 +24,8 @@ class Prover:
             return False
 
     def assumption(self) -> bool:
-        self.__set_undo()
         if self.goal in self.variables:
+            self.__set_undo()
             self.goal = None
             if self.subgoals:
                 (self.goal, self.variables) = self.subgoals.pop()
@@ -34,8 +34,8 @@ class Prover:
             return True
 
     def intro(self) -> bool:
-        self.__set_undo()
         if self.goal.symbol == then_symbol:
+            self.__set_undo()
             self.variables.append(self.goal.left)
             self.goal = self.goal.right
             return False
@@ -43,19 +43,19 @@ class Prover:
             return True
 
     def apply(self, number) -> bool:
-        self.__set_undo()
         if len(self.variables) <= number:
             return True
         elif (self.variables[number].symbol == then_symbol
               and self.variables[number].right == self.goal):
+            self.__set_undo()
             self.goal = self.variables[number].left
             return False
         else:
             return True
 
     def split(self) -> bool:
-        self.__set_undo()
         if self.goal.symbol == and_symbol:
+            self.__set_undo()
             self.subgoals.append((self.goal.right, self.variables))
             self.goal = self.goal.left
             return False
@@ -63,31 +63,32 @@ class Prover:
             return True
 
     def left(self):
-        self.__set_undo()
         if self.goal.symbol == or_symbol:
+            self.__set_undo()
             self.goal = self.goal.left
             return False
         else:
             return True
 
     def right(self):
-        self.__set_undo()
         if self.goal.symbol == or_symbol:
+            self.__set_undo()
             self.goal = self.goal.right
             return False
         else:
             return True
 
     def destruct(self, number) -> bool:
-        self.__set_undo()
         if len(self.variables) <= number:
             return True
         elif self.variables[number].symbol == and_symbol:
+            self.__set_undo()
             self.variables.append(self.variables[number].left)
             self.variables.append(self.variables[number].right)
             del self.variables[number]
             return False
         elif self.variables[number].symbol == or_symbol:
+            self.__set_undo()
             temp = self.variables[:]
             temp[number] = temp[number].right
             self.subgoals.append((self.goal, temp))
@@ -97,13 +98,13 @@ class Prover:
             return True
 
     def specialize(self, function, domain) -> bool:
-        self.__set_undo()
         if len(self.variables) <= min(function, domain):
             return True
         elif (self.variables[function].symbol != "→"
               or self.variables[function].left != self.variables[domain]):
             return True
         else:
+            self.__set_undo()
             self.variables[function] = self.variables[function].right
             return False
 
